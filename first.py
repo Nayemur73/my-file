188569a35c12a11a33abcc20c43d8219188e4843
Nayemur73 <nayemurrahmanraju73@gmail.com>
2025-11-26 19:56:57 +0600

    duplicate

---
+++ first.py
@@ -0,0 +1,19 @@
+print("hello world !")
+x=10
+y=20
+
+sum=x+y
+sub=y-x
+mul=x*y
+div=x/y
+
+all=[sub,mul,div]
+print(all[0])
+
+# print(sum)
+# print(sub)
+# print(mul)
+# print(div)
+
+x_y="hello"
+
